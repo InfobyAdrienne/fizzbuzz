5fc4380ac45d7d3cb0d0b6bf87ce433fe37a01fe
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-08-20 16:11:24 +0100

    Edited main and spec fizzbuzz documents

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,27 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
-      expect(fizzbuzz(3)).to eq 'fizz'
+    it 'returns "Fizz" when passed 3' do
+      expect(fizzbuzz(3)).to eq 'Fizz'
     end
   end  
    
 describe 'fizzbuzz' do
-    it 'returns "buzz" when passed 5' do
-      expect(fizzbuzz(5)).to eq 'buzz'
+    it 'returns "Buzz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'Buzz'
     end
-  end  
+end
+
+
+describe 'fizzbuzz' do
+    it 'returns "FizzBuzz" when passed 5 and 3' do
+        expect(fizzbuzz(15)).to eq 'FizzBuzz'
+     end
+end
+
+
+describe 'fizzbuzz' do
+    it 'returns "number" when not passed either 5 or 3' do
+        expect(fizzbuzz(2)).to eq 2
+    end
+end 
